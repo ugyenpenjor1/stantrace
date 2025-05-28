c1ca4132f22adfc2f09531a1a92c1a7201ebcfc6
Ugyen Penjor <ugyenpenjor.bt@gmail.com>
2025-05-28 23:48:26 +0100

    Added space after @export

--- R/plot_ppc.R
+++ R/plot_ppc.R
@@ -6,28 +6,29 @@
 #'
 #' @return A ggplot2 object showing the PPC scatter plot.
 #' @export
+
 plot_ppc <- function(fit, obs_var = "T_obs_sum", sim_var = "T_sim_sum") {
   # Load draws
   draws_df <- fit$draws(variables = c(obs_var, sim_var), format = "data.frame")
-  
+
   # Extract vectors
   fit_vals <- draws_df[[obs_var]]
   fit_rep_vals <- draws_df[[sim_var]]
-  
+
   # Create data frame for plotting
   ppc_df <- data.frame(
     fit = fit_vals,
     fit_rep = fit_rep_vals,
     colour = ifelse(fit_rep_vals > fit_vals, "salmon", "steelblue")
   )
-  
+
   # Axis limits
   lims <- range(c(fit_vals, fit_rep_vals))
-  
+
   # Compute Bayesian p-value
   bpv <- mean(fit_rep_vals > fit_vals)
   bpv_label <- paste0("italic(P) == ", round(bpv, 2))
-  
+
   # Generate plot
   ggplot2::ggplot(ppc_df, ggplot2::aes(x = fit, y = fit_rep)) +
     ggplot2::geom_point(shape = 21, size = 2, fill = ppc_df$colour, color = 'black') +
